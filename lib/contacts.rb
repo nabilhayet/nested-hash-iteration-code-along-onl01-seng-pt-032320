0344b9801c9fbb1bf1f1520c37c428516540740f
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-02 03:12:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,6 @@ contacts = {
 def remove_strawberry(contacts)
   contacts.each do |person,contact_details_hash|
     if person== "Freddy Mercury"
-      contact_details_hash.each do ||
+      contact_details_hash.each do |attribute, data|
+        binding.pry
 end
