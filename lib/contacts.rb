ff398a10a3d7e0dff407d15b866c8bf8f8f75651
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-02 03:13:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,6 @@ def remove_strawberry(contacts)
   contacts.each do |person,contact_details_hash|
     if person== "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        if attribute== :
+        if attribute== ::favorite_ice_cream_flavors
         binding.pry
 end
